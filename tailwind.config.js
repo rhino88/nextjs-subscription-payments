add84705dfcca62c68804e75a42455195c976e0d
Ryan Anderson <andersra88@gmail.com>
2022-09-07 21:19:39 -0400

    add tailwind css plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,6 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
-  ]
+  ],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')]
 };
